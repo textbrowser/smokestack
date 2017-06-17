507609333b4313fee07472c8017766b9ccd027fd
Monster <textbrowser@gmail.com>
2017-06-17 09:16:47 -0400

    The exclusive transaction does not prevent the duplicate messages.

--- SmokeStack/app/src/main/java/org/purple/smokestack/Database.java
+++ SmokeStack/app/src/main/java/org/purple/smokestack/Database.java
@@ -3072,7 +3072,7 @@ public class Database extends SQLiteOpenHelper
 	if(m_db == null)
 	    return;
 
-	m_db.beginTransaction(); // Exclusive transaction.
+	m_db.beginTransactionNonExclusive();
 
 	try
 	{
