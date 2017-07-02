981136bbfc7d4b434809c1a00660469c152d548b
Monster <textbrowser@gmail.com>
2017-07-01 20:46:55 -0400

    Removed imports.

--- SmokeStack/app/src/main/java/org/purple/smokestack/Neighbor.java
+++ SmokeStack/app/src/main/java/org/purple/smokestack/Neighbor.java
@@ -29,7 +29,6 @@ package org.purple.smokestack;
 
 import android.util.Base64;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
