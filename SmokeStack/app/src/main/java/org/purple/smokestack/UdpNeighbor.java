4aa57d15e4fd2dfcce973e5e0c0efe7d24977e7b
Monster <textbrowser@gmail.com>
2018-03-02 10:07:45 -0500

    Zero bytes may be read.

--- SmokeStack/app/src/main/java/org/purple/smokestack/UdpNeighbor.java
+++ SmokeStack/app/src/main/java/org/purple/smokestack/UdpNeighbor.java
@@ -191,6 +191,8 @@ public class UdpNeighbor extends Neighbor
 			disconnect();
 			return;
 		    }
+		    else if(bytesRead == 0)
+			return;
 
 		    m_bytesRead.getAndAdd(bytesRead);
 		    m_lastTimeRead.set(System.nanoTime());
