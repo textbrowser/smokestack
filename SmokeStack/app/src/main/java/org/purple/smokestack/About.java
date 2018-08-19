f81f84e3c71573c864eaa1ba841eb64350de60c4
Monster <textbrowser@gmail.com>
2018-08-19 08:11:29 -0400

    Next release.

--- SmokeStack/app/src/main/java/org/purple/smokestack/About.java
+++ SmokeStack/app/src/main/java/org/purple/smokestack/About.java
@@ -48,7 +48,7 @@ public class About
 		SimpleDateFormat("yyyy-MM-dd h:mm:ss");
 
 	    simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-            s_about = "Version 2018.08.19 Blooming Bee " +
+            s_about = "Version 2018.09.01 Drawing Drums " +
                 (BuildConfig.DEBUG ? "(Debug) " : "(Release)") +
                 "\nBuild Date " +
                 simpleDateFormat.format(new Date(BuildConfig.BUILD_TIME)) +
