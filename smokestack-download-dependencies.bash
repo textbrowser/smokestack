d3ab8d2fd6cc70833ddba6d151e8ecd13620f070
textbrowser@gmail.com <textbrowser@gmail.com>
2020-06-08 15:10:14 -0400

    Echo.

--- smokestack-download-dependencies.bash
+++ smokestack-download-dependencies.bash
@@ -11,3 +11,4 @@ wget --progress=bar https://bouncycastle.org/download/$bouncycastle1
 wget --progress=bar https://bouncycastle.org/download/$bouncycastle2
 mv $bouncycastle1 SmokeStack/app/libs/.
 mv $bouncycastle2 SmokeStack/app/libs/.
+echo "Please review SmokeStack/app/build.gradle!" 
