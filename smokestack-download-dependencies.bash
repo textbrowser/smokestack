ddf0cf493c212fe23b22dddb5119bfe3b4ef012b
textbrowser@gmail.com <textbrowser@gmail.com>
2020-06-11 14:36:41 -0400

    BC source files.

--- smokestack-download-dependencies.bash
+++ smokestack-download-dependencies.bash
@@ -5,10 +5,16 @@
 # Bouncy Castle
 
 bouncycastle1=bcpkix-jdk15on-165.jar
-bouncycastle2=bcprov-ext-jdk15on-165.jar
+bouncycastle2=bcpkix-jdk15on-165.tar.gz
+bouncycastle3=bcprov-ext-jdk15on-165.jar
+bouncycastle4=bcprov-jdk15on-165.tar.gz
 
 wget --progress=bar https://bouncycastle.org/download/$bouncycastle1
 wget --progress=bar https://bouncycastle.org/download/$bouncycastle2
+wget --progress=bar https://bouncycastle.org/download/$bouncycastle3
+wget --progress=bar https://bouncycastle.org/download/$bouncycastle4
 mv $bouncycastle1 SmokeStack/app/libs/.
 mv $bouncycastle2 SmokeStack/app/libs/.
+mv $bouncycastle3 SmokeStack/app/libs/.
+mv $bouncycastle4 SmokeStack/app/libs/.
 echo "Please review SmokeStack/app/build.gradle!" 
