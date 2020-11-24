f8720c928489f8ed739d68df743f7a73c2a93190
textbrowser@gmail.com <textbrowser@gmail.com>
2020-11-24 13:29:06 -0500

    Removed BC source.

--- smokestack-download-dependencies.bash
+++ smokestack-download-dependencies.bash
@@ -5,18 +5,12 @@
 # Bouncy Castle
 
 bouncycastle1=bcpkix-jdk15on-167.jar
-bouncycastle2=bcpkix-jdk15on-167.tar.gz
-bouncycastle3=bcprov-ext-jdk15on-167.jar
-bouncycastle4=bcprov-jdk15on-167.tar.gz
+bouncycastle2=bcprov-ext-jdk15on-167.jar
 
 rm -f $bouncycastle1
 rm -f $bouncycastle2
-rm -f $bouncycastle3
-rm -f $bouncycastle4
 wget --progress=bar https://bouncycastle.org/download/$bouncycastle1
 wget --progress=bar https://bouncycastle.org/download/$bouncycastle2
-wget --progress=bar https://bouncycastle.org/download/$bouncycastle3
-wget --progress=bar https://bouncycastle.org/download/$bouncycastle4
 
 if [ -r "$bouncycastle1" ]; then
     mv $bouncycastle1 SmokeStack/app/libs/.
@@ -28,18 +22,6 @@ if [ -r "$bouncycastle2" ]; then
     mv $bouncycastle2 SmokeStack/app/libs/.
 else
     echo "Cannot read $bouncycastle2."
-fi
-
-if [ -r "$bouncycastle3" ]; then
-    mv $bouncycastle3 SmokeStack/app/libs/.
-else
-    echo "Cannot read $bouncycastle3."
-fi
-
-if [ -r "$bouncycastle4" ]; then
-    mv $bouncycastle4 SmokeStack/app/libs/.
-else
-    echo "Cannot read $bouncycastle4."
 fi
 
 echo "Please review SmokeStack/app/build.gradle and SmokeStack/app/libs!"
