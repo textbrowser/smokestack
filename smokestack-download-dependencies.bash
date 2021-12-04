20e684291d89748dd1446133187e136b19e69ba9
textbrowser@gmail.com <textbrowser@gmail.com>
2021-12-04 15:05:32 -0500

    Bouncy Castle 1.70.

--- smokestack-download-dependencies.bash
+++ smokestack-download-dependencies.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
+# Alexis Megas.
 
 # Must be executed in the top-level source directory.
 
 # Bouncy Castle
 
-bouncycastle1=bcpkix-jdk15on-169.jar
-bouncycastle2=bcprov-ext-jdk15on-169.jar
+bouncycastle1=bcpkix-jdk15on-170.jar
+bouncycastle2=bcprov-ext-jdk15on-170.jar
 
 rm -f $bouncycastle1
 rm -f $bouncycastle2
@@ -13,12 +14,14 @@ wget --progress=bar https://bouncycastle.org/download/$bouncycastle1
 wget --progress=bar https://bouncycastle.org/download/$bouncycastle2
 
 if [ -r "$bouncycastle1" ]; then
+    mkdir -p SmokeStack/app/libs
     mv $bouncycastle1 SmokeStack/app/libs/.
 else
     echo "Cannot read $bouncycastle1."
 fi
 
 if [ -r "$bouncycastle2" ]; then
+    mkdir -p SmokeStack/app/libs
     mv $bouncycastle2 SmokeStack/app/libs/.
 else
     echo "Cannot read $bouncycastle2."
